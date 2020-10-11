8645b30a69913db2510a3df0b38249c6a1757644
Eric Cuellar <43803772+ericphotons@users.noreply.github.com>
2020-10-11 01:43:45 -0700

    Update USSD-config.php
    deleted api key

--- USSD-config.php
+++ USSD-config.php
@@ -2,6 +2,6 @@
 
 // Specify Africa's Talking login credentials
 $username   = "sandbox";   // CHANGE ONCE LIVE
-$apikey     = "20073dc7b9ad717ff085910a2befa125314ac5a70a6053bff685c1b5c0172949";  //
+$apikey     = "";  // INSERT YOUR API KEY
 
 ?>
