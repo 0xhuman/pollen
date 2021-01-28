29c925015f7d0f2e0758c34347acb0704d54b7fa
Eric Cuellar <43803772+ericphotons@users.noreply.github.com>
2021-01-27 19:26:17 -0800

    Testing Fixie for Heroku Static IP

--- Dashboard-googleCharts.php
+++ Dashboard-googleCharts.php
@@ -1,5 +1,22 @@
 <?php 
     require_once ('USSD-dbConnect.php');
+
+    function proxyRequest() {
+        $fixieUrl = getenv("FIXIE_URL");
+        $parsedFixieUrl = parse_url($fixieUrl);
+
+        $proxy = $parsedFixieUrl['host'].":".$parsedFixieUrl['port'];
+        $proxyAuth = $parsedFixieUrl['user'].":".$parsedFixieUrl['pass'];
+
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_PROXY, $proxy);
+        curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyAuth);
+        curl_close($ch);
+      }
+
+      $response = proxyRequest();
+      print_r($response);
 ?>
 
 <html>
